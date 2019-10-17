f694de0ead520c96edbc769baa2bae43f5b44683
nicolasnacosta <56615682+nicolasnacosta@users.noreply.github.com>
2019-10-16 22:05:28 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:radio/src/pages/home_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: '/',
+      routes: {
+        '/'   : ( BuildContext context ) => HomePage()
+      },
+    );
+  }
+}
